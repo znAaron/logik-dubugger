9644a00433fd92a4736a95b2219f1d325246d04c
Zinan Guo <guozinan2000@gmail.com>
2025-04-07 22:19:03 -0400

    init

---
+++ assets/side_panel/media/main.js
@@ -0,0 +1,59 @@
+// @ts-nocheck
+
+// This script will be run within the webview itself
+// It cannot access the main VS Code APIs directly.
+(function () {
+    const vscode = acquireVsCodeApi();
+
+    const oldState = vscode.getState() || { colors: [] };
+
+    /** @type {Array<{ value: string }>} */
+    let colors = oldState.colors;
+
+
+    document.querySelector('.debug-btn').addEventListener('click', () => {
+        vscode.postMessage({ type: 'debug', value: "test2.js" });
+    });
+
+    /**
+     * @param {Array<{ value: string }>} colors
+     */
+    function updateColorList(colors) {
+        const ul = document.querySelector('.color-list');
+        ul.textContent = '';
+        for (const color of colors) {
+            const li = document.createElement('li');
+            li.className = 'color-entry';
+
+            const colorPreview = document.createElement('div');
+            colorPreview.className = 'color-preview';
+            colorPreview.style.backgroundColor = `#${color.value}`;
+            colorPreview.addEventListener('click', () => {
+                onColorClicked(color.value);
+            });
+            li.appendChild(colorPreview);
+
+            const input = document.createElement('input');
+            input.className = 'color-input';
+            input.type = 'text';
+            input.value = color.value;
+            input.addEventListener('change', (e) => {
+                const value = e.target.value;
+                if (!value) {
+                    // Treat empty value as delete
+                    colors.splice(colors.indexOf(color), 1);
+                } else {
+                    color.value = value;
+                }
+            });
+            li.appendChild(input);
+
+            ul.appendChild(li);
+        }
+
+        // Update the saved state
+        vscode.setState({ colors: colors });
+    }
+}());
+
+
